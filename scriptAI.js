485dc323a383d62e7fd97d23d836f14b2064b97c
Memaybew-1 <memaybewv1@gmail.com>
2026-06-23 18:39:06 +0700

    Modify display message after learning new animal
    Updated display message for learning new animal.

--- scriptAI.js
+++ scriptAI.js
@@ -6,22 +6,27 @@ function display(text) {
 
 function play() {
     let node = brain;
+    // Đi theo câu hỏi cho đến khi gặp đáp án (node không còn question)
     while (node.question) {
         node = confirm(node.question) ? node.yes : node.no;
     }
     
+    // Đoán đáp án cuối cùng
     if (confirm("Có phải là " + node.answer + " không?")) {
         display("Tôi thắng rồi! Đó là " + node.answer);
     } else {
+        // Dạy AI
         let newAnimal = prompt("Tiếc quá, đó là con gì?");
         let newQuestion = prompt("Câu hỏi để phân biệt " + newAnimal + " với " + node.answer + "?");
         
         let oldAnswer = node.answer;
+        // Biến node đáp án thành câu hỏi
         node.question = newQuestion;
         node.yes = { answer: newAnimal };
         node.no = { answer: oldAnswer };
         delete node.answer;
-        display("Cảm ơn, tôi đã học thêm được về " + newAnimal + "!");
+        
+        display("Cảm ơn! Tôi đã ghi nhớ thêm về " + newAnimal);
     }
 }
 
